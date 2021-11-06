f16f020b137c8944f170082f5eb56e026fc53441
Déborah Ballaminut <deborah.balla@gmail.com>
2021-11-06 12:56:36 -0300

    Commit Initial

---
+++ Exemplo1-Teste/Operacoes-UnitTest.cs
@@ -0,0 +1,77 @@
+﻿using Exemplo1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Exemplo1_Teste
+{
+    public class Operacoes_UnitTest
+    {
+        [Fact]
+        public void deveSomarDoubleRetornarDouble()
+        {
+            //Arrange
+            var valor1 = 5.9;
+            var valor2 = 9.1;
+            var esperado = 15;
+
+            //Act
+            var obtido = Operacoes.somar(valor1, valor2);
+
+            //Assert
+            Assert.Equal(esperado, obtido);
+
+        }
+
+        [Fact]
+        public void deveSubtrairDoubleRetornarDoube()
+        {
+            //Arrange
+            var valor1 = 10.5;
+            var valor2 = 9.5;
+            var esperado = 1;
+
+            //Act
+            var obtido = Operacoes.subtrair(valor1, valor2);
+
+            //Assert
+            Assert.Equal(esperado, obtido);
+
+        }
+
+        [Fact]
+        public void deveMultiplicarDoubleRetornarDoube()
+        {
+            //Arrange
+            var valor1 = 2.36;
+            var valor2 = 5.81;
+            var esperado = 13.71;
+
+            //Act
+            var obtido = Operacoes.multiplicar(valor1, valor2);
+
+            //Assert
+            Assert.Equal(esperado, obtido, 2);
+
+        }
+
+        [Fact]
+        public void deveDividirrDoubleRetornarDoube()
+        {
+            //Arrange
+            var valor1 = 4.44;
+            var valor2 = 2.22;
+            var esperado = 2;
+
+            //Act
+            var obtido = Operacoes.dividir(valor1, valor2);
+
+            //Assert
+            Assert.Equal(esperado, obtido, 2);
+
+        }
+    }
+}
---
+++ Exemplo1/Operacoes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    public static class Operacoes
+    {
+        public static double somar(double n1, double n2)
+        {
+            return (n1 + n2);
+        }
+        public static double subtrair(double n1, double n2)
+        {
+            return (n1 - n2);
+        }
+        public static double multiplicar(double n1, double n2)
+        {
+            return (n1 * n2);
+        }
+        public static double dividir(double n1, double n2)
+        {
+            return (n1 / n2);
+        }
+    }
+}
